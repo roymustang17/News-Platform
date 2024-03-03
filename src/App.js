321758505177f9215823841571054c11106eadbd
Arjun A <arjunaggarwal1710@gmail.com>
2024-03-03 18:36:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const fetchAPI = async () => {
+  const url =
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=41e06e4589bc499d840c6f5aad1630e2";
+  const data = await fetch(url);
+  const parsedData = await data.json();
+  console.log(parsedData);
+  return parsedData.articles;
+};
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const newsArticles = await fetchAPI();
+      setArticles(newsArticles.slice(0, 3));
+    };
+
+    fetchData();
+    return () => {};
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className="display">
+        {articles.map((article, index) => (
+          <div key={index}>
+            <a href={article.url} target="_blank" className="titles">
+              <h1 className="titles">{article.title}</h1>
+            </a>
+            {/* <p>{article.description}</p> */}
+            <img src={article.urlToImage} alt="image" className="images" />
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
